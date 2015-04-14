f6313d76b6ddda50c8085b683134ec0071c44524
2289034325 <2289034325@qq.com>
2015-04-14 23:08:13 +0800

    细分

---
+++ BIANMA/Form_Bianma.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BIANMA
+{
+    public partial class Form_Bianma : Form
+    {
+        public Form_Bianma()
+        {
+            InitializeComponent();
+        }
+    }
+}
