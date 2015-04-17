ea05101e35bed5d671f543061e71289694b593eb
2289034325 <2289034325@qq.com>
2015-04-17 19:27:56 +0800

    追加

---
+++ JCSJZX/Page_Users.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace JCSJZX
+{
+    public partial class Page_Users : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (IsPostBack)
+            { }
+            else
+            {
+                //加载除了管理员的所有用户
+                Tool.DB.JCSJ.OPT db = new Tool.DB.JCSJ.OPT();
+                grid_users.DataSource = db.GetAllUsers(false);
+            }
+        }
+    }
+}
---
+++ Tool/CommonFunc.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tool
+{
+    public class CommonFunc
+    {
+
+        /// <summary>
+        /// MD5加密
+        /// </summary>
+        /// <param name="str">源字符串</param>
+        /// <returns></returns>
+        public static string MD5_16(string str)
+        {
+            MD5 md5 = new MD5CryptoServiceProvider();
+            string mmd5 = BitConverter.ToString(md5.ComputeHash(Encoding.UTF8.GetBytes(str))).Replace("-", "");
+
+            return mmd5.Substring(8, 16);
+        }
+    }
+}
