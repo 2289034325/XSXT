c0306e58070a37ab71bcedf2548108197bd81acc
2289034325 <2289034325@qq.com>
2015-07-26 06:26:35 +0800

    修改移动端的js

--- JCSJGL/MobileAdapter.js
+++ JCSJGL/MobileAdapter.js
@@ -61,30 +61,30 @@ $(document).ready(function () {
             $.event.special.swipe.verticalDistanceThreshold = 50;
         });
 
+        //Iphone webApp
+        $("head").append("<meta name='viewport' content='user-scalable=yes, initial-scale=1.0, width=device-width, minimal-ui'>");
+        $("head").append("<meta name='apple-mobile-web-app-status-bar-style' content='black'>");
+        $("head").append("<meta name='apple-mobile-web-app-capable' content='yes'>");
+        $("head").append("<meta name='format-detection' content='telephone=no'>");
+        $("head").append("<link rel='apple-touch-icon' sizes='114x114' href='imgs/iphoneLogo.png' />");
+        //拦截超链接
+        $("a").click(function (event) {
+            if (this.href != "#" && this.href) {
+                event.preventDefault(); window.document.location.href = this.href;
+            }
+        });
+
+        //将包含子菜单的一级菜单的链接上的onclick去掉
+        $("#mn_main a[href='#']").removeAttr("onclick").removeAttr("href");
+        //将当前菜单的字体加粗
+        var cName = $(window.location.pathname.split("/")).last()[0];
+        var mni = $("#mn_main a[href='" + cName + "']");
+        $("#mn_main > ul > li").has(mni).find(">a").css("font-weight", "bold");
+        mni.css("font-weight", "bold");
+
         //加载jquerymobile
         $.getScript("jquery.mobile-1.4.5/jquery.mobile-1.4.5.js", function ()
         {
-            //Iphone webApp
-            $("head").append("<meta name='viewport' content='user-scalable=yes, initial-scale=1.0, width=device-width, minimal-ui'>");
-            $("head").append("<meta name='apple-mobile-web-app-status-bar-style' content='black'>");
-            $("head").append("<meta name='apple-mobile-web-app-capable' content='yes'>");
-            $("head").append("<meta name='format-detection' content='telephone=no'>");
-            $("head").append("<link rel='apple-touch-icon' sizes='114x114' href='imgs/iphoneLogo.png' />");
-            //拦截超链接
-            $("a").click(function (event) {
-                if (this.href != "#" && this.href) {
-                    event.preventDefault(); window.document.location.href = this.href;
-                }
-            });
-
-            //将包含子菜单的一级菜单的链接上的onclick去掉
-            $("#mn_main a[href='#']").removeAttr("onclick").removeAttr("href");
-            //将当前菜单的字体加粗
-            var cName = $(window.location.pathname.split("/")).last()[0];
-            var mni = $("#mn_main a[href='" + cName + "']");
-            $("#mn_main > ul > li").has(mni).find(">a").css("font-weight", "bold");
-            mni.css("font-weight", "bold");
-
             //左右滑动，代表后退前进
             $(document).on("swipeleft", function () {
                 history.back();
@@ -93,35 +93,5 @@ $(document).ready(function () {
                 history.forward();
             });
         });
-
-
-        //Iphone webApp
-        //$("head").append("<meta name='viewport' content='user-scalable=yes, initial-scale=1.0, width=device-width, minimal-ui'>");
-        //$("head").append("<meta name='apple-mobile-web-app-status-bar-style' content='black'>");
-        //$("head").append("<meta name='apple-mobile-web-app-capable' content='yes'>");
-        //$("head").append("<meta name='format-detection' content='telephone=no'>");
-        //$("head").append("<link rel='apple-touch-icon' sizes='114x114' href='imgs/iphoneLogo.png' />");
-        ////拦截超链接
-        //$("a").click(function (event) {
-        //    if (this.href != "#" && this.href) {
-        //        event.preventDefault(); window.document.location.href = this.href;
-        //    }
-        //});
-
-        ////将包含子菜单的一级菜单的链接上的onclick去掉
-        //$("#mn_main a[href='#']").removeAttr("onclick").removeAttr("href");
-        ////将当前菜单的字体加粗
-        //var cName = $(window.location.pathname.split("/")).last()[0];
-        //var mni = $("#mn_main a[href='" + cName + "']");
-        //$("#mn_main > ul > li").has(mni).find(">a").css("font-weight", "bold");
-        //mni.css("font-weight", "bold");
-
-        ////左右滑动，代表后退前进
-        //$("body").on("swipeleft", function () {
-        //    history.back();
-        //});
-        //$("body").on("swiperight", function () {
-        //    history.forward();
-        //});
     }
 });
